95f0b7411834a864b8366b8ba74d1b8071f65e6c
BolverBlitz <35345288+BolverBlitz@users.noreply.github.com>
2020-12-22 19:23:36 +0100

    getstops

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# oepnv-nuremberg
- A wrapper for multiple datapoints from VAG and VGN
+# oepnv-nuremberg  
+ A wrapper for multiple datapoints from VAG and VGN  
+  
+## Where is the data comming from?  
+ Departures and stops are comming from the official VAG API endpoint  
+ URL: https://opendata.vag.de/  
+ License: Creative Commons Attribution 4.0 Int.  
+  
+ Stops based on an adress  comee from an undocumented endpoint of the VGN  
+ URL: HPSSuggest.php  
+ License: Unknown  
+  
+ Connections between stops come from a web scraper directly from VGN.de.  
+ URL: https://www.vgn.de/verbindungen/  
+ License: Unknown  
+
+### Functions and todo list
+
+- [ ] Departures
+- [ ] Stops based on names
+- [ ] Stops based on GPS
+- [ ] Cacheing of Stops
+- [ ] Stops based on IDs (Not supported by API, will use Cache)
+- [ ] Turn adress into GPS
+- [ ] Turn adress into list of near stops
+- [ ] Get routes from and to stops
+- [ ] Get routes to anything. IDs, Stopnames, Adresses, GPS
+  
+### Usage
+ > A working example is [Test.js](URL)
+
+ Usage:
+ ```js
+ const vgn = require('oepnv-nuremberg');
+
+ var Name = 'Opern';
+ vgn.Haltestellen(Name).then(
+    function(message) {
+     console.log(message);
+    });
+ ```
---
+++ index.js
@@ -0,0 +1,43 @@
+const VAGDE = "https://start.vag.de/dm/api";
+//https://start.vag.de/dm/api/haltestellen.json/vgn?name=Plärrer
+
+const HPSSuggest = "https://www.vgn.de/ib/site/tools/HPSSuggest.php";
+//https://www.vgn.de/ib/site/tools/HPSSuggest.php?query={QueryString}&gps=1&Edition=de
+
+const VGNDE = "https://www.vgn.de/verbindungen/";
+//https://www.vgn.de/verbindungen/?to=de%3A09564%3A704&td=coord%3A4440209.72739%3A686069.44855%3ANAV4%3AN%C3%BCrnberg%2C%20Leithastra%C3%9Fe
+
+const request = require("request");
+
+function urlReformat(value)
+{
+    value = value.replace(/ä/g, "%C3%A4");
+    value = value.replace(/ö/g, "%C3%B6");
+    value = value.replace(/ü/g, "%C3%BC");
+    return value;
+}
+
+/* Haltestellen VAG API */
+let getstops = function(Name) {
+	return new Promise(function(resolve, reject) {
+		var url = VAGDE + "/haltestellen.json/vgn?name=" + urlReformat(Name.trim());
+		request(url, { json: true }, (err, res, body) => {
+			if (err) { reject(err); }
+			for(i in body.Haltestellen){
+				let HaltestellennameSplit = body.Haltestellen[i].Haltestellenname.split("(");
+				let Name = HaltestellennameSplit[0];
+				body.Haltestellen[i].Haltestellenname = Name.trim();
+				let Ort = HaltestellennameSplit[1].replace(/[)]/g,"",);
+				body.Haltestellen[i].Ort = Ort;
+				body.Haltestellen[i].Produkte = body.Haltestellen[i].Produkte.replace(/ubahn/i,"U-Bahn",);
+				body.Haltestellen[i].Produkte = body.Haltestellen[i].Produkte.replace(/,/g,", ",);
+			}
+			resolve(body.Haltestellen);
+  
+		});
+	});
+}
+
+module.exports = {
+	getstops
+};
