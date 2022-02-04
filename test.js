fe3497f2a923e36fc954f37e7ac486e91f99cac6
BolverBlitz <bolver.blitz@gmail.com>
2022-02-04 06:02:09 +0100

    Fixed Testing URL

--- test.js
+++ test.js
@@ -38,7 +38,7 @@ Fahrzeugnummer
         //const Output = await vgn.getStops('Hardhöhe', {limit: 10});
         //const Output = await vgn.getStopsbygps('49.45015694', '11.083455', {limit: 2, distance: 400, sort: 'Distance'});
         const Output = await vgn.getDepartures("PLAE", {Product: "Tram", TimeSpan: 60, TimeDelay: 0, LimitCount: 5})
-        const OutputCompare = await askURL(`https://start.vag.de/dm/api/abfahrten.json/PLAE/PL/4?Product=Tram&TimeSpan=60&TimeDelay=0&LimitCount=5`)
+        const OutputCompare = await askURL(`https://start.vag.de/dm/api/abfahrten.json/VAG/PLAE/4?Product=Tram&TimeSpan=60&TimeDelay=0&LimitCount=5`)
         //const Output = await vgn.getDeparturesbygps('49.4480881582118', '11.0647882822154', {Product: "ubahn", TimeSpan: 20, TimeDelay: 0, LimitCount: 5, limit: 100, distance: 500, sort: 'Distance'})
         console.log("Should only contain departures from line 4")
         console.log(OutputCompare.Abfahrten)
