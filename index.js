99f19496a97c820651e8cf90e9de320a3f70e83a
BolverBlitz <bolver.blitz@gmail.com>
2022-02-18 02:21:12 +0100

    Formating fix

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Haltestellen = require('./src/haltestellen');
-const Abfahrten = require('./src/abfahrten');
-const Fahrten = require('./src/fahrten');
-const WebProcessor = require('./src/web_processor');
+const Haltestellen = require("./src/haltestellen");
+const Abfahrten = require("./src/abfahrten");
+const Fahrten = require("./src/fahrten");
+const WebProcessor = require("./src/web_processor");
 const allowed_apiparameter = {
-    Departures: ['product', 'timespan', 'timedelay', 'limitcount'],
-    Stops: ['name', 'lon', 'lat', 'distance'],
-    Trips: ['timespan']
+    Departures: ["product", "timespan", "timedelay", "limitcount"],
+    Stops: ["name", "lon", "lat", "distance"],
+    Trips: ["timespan"]
     };
 
 class openvgn {
@@ -17,7 +17,7 @@ class openvgn {
     constructor(api_url, vag_url) {
         this.api_url = api_url || "https://start.vag.de/dm/api";
         this.vag_url = vag_url || "https://apigateway.vag.de/efa/";
-    }
+    };
 
     urlReformat(value)
     {
@@ -25,7 +25,7 @@ class openvgn {
         value = value.replace(/ö/g, "%C3%B6");
         value = value.replace(/ü/g, "%C3%BC");
         return value;
-    }
+    };
     /**
      * 
      * @param {Array} data 
@@ -34,25 +34,26 @@ class openvgn {
      */
     encodeQueryData(data, endpoint) {
         const ret = [];
-        for (let d in data)
-        if(allowed_apiparameter[endpoint].includes(encodeURIComponent(d).toLowerCase())){
-            ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
-        }
-        return ret.join('&');
-    }
+        for (let d in data){
+            if(allowed_apiparameter[endpoint].includes(encodeURIComponent(d).toLowerCase())){
+                ret.push(encodeURIComponent(d) + "=" + encodeURIComponent(data[d]));
+            };
+        };
+        return ret.join("&");
+    };
 
     /**
      * @param {String} target Stop name
      * @param {Object} parameter Quary parameter
     */
     getStops(target, parameter) {
-        const url = `${this.api_url}/haltestellen.json/vgn?name=${this.urlReformat(target.trim())}`
+        const url = `${this.api_url}/haltestellen.json/vgn?name=${this.urlReformat(target.trim())}`;
         return Haltestellen.getStops(url, parameter).then(function(Haltestellen){
             return Haltestellen
         }).catch(function(err){
-            return err
-        })
-    }
+            return err;
+        });
+    };
 
     /**
      * @param {String} lat GPS Lat
@@ -62,37 +63,37 @@ class openvgn {
     getStopsbygps(lat, lon, parameter){
         if(!parameter.distance){
 			parameter.distance = 500;
-		}
+		};
 		if(!parameter.sort){
 			parameter.sort = "Distance";
-		}
-        const url = `${this.api_url}/haltestellen.json/vgn?lon=${lon}&lat=${lat}&Distance=${parameter.distance}`
+		};
+        const url = `${this.api_url}/haltestellen.json/vgn?lon=${lon}&lat=${lat}&Distance=${parameter.distance}`;
         return Haltestellen.getStopsbygps(url, lat, lon,parameter).then(function(Haltestellen){
-            return Haltestellen
+            return Haltestellen;
         }).catch(function(err){
-            return err
-        })
-    }
+            return err;
+        });
+    };
 
     /**
      * @param {String} target Stop ID or VAG haltid
      * @param {Object} parameter Quary parameter
     */
     getDepartures(target, parameter){
-        let source = "vgn"
+        let source = "vgn";
         if(isNaN(target)){
-            source = "vag"
-        }
-        let url = `${this.api_url}/abfahrten.json/${source}/${target}`
+            source = "vag";
+        };
+        let url = `${this.api_url}/abfahrten.json/${source}/${target}`;
         if(parameter){
-			url = `${url}?${this.encodeQueryData(parameter, 'Departures')}`
-		}
+			url = `${url}?${this.encodeQueryData(parameter, "Departures")}`;
+		};
         return Abfahrten.getDepartures(url, parameter).then(function(Abfahrten){
-            return Abfahrten
+            return Abfahrten;
         }).catch(function(err){
-            return err
-        })
-    }
+            return err;
+        });
+    };
 
     /**
      * 
@@ -104,17 +105,17 @@ class openvgn {
     getDeparturesbygps(lat, lon, parameter){
         if(!parameter.distance){
             parameter.distance = 500;
-        }
+        };
         if(!parameter.sort){
             parameter.sort = "Distance";
-        }
-        const url = `${this.api_url}/haltestellen.json/vgn?lon=${lon}&lat=${lat}&Distance=${parameter.distance}`
+        };
+        const url = `${this.api_url}/haltestellen.json/vgn?lon=${lon}&lat=${lat}&Distance=${parameter.distance}`;
         return Abfahrten.getDeparturesbygps(url, lat, lon, parameter, this.api_url, this.encodeQueryData).then(function(Abfahrten){
-            return Abfahrten
+            return Abfahrten;
         }).catch(function(err){
-            return err
-        })
-    }
+            return err;
+        });
+    };
 
     /**
      * @param {Number} Fahrtnummer Fahrtnummer
@@ -123,35 +124,34 @@ class openvgn {
      getTrip(Fahrtnummer, parameter){
         let url;
         if(parameter.date){
-            const date = new Date(parameter.date).toLocaleTimeString('de-DE', {day: "2-digit", month: "2-digit", year: "numeric"}).split(",")[0]
-            console.log(date)
-            url = `${this.api_url}/fahrten.json/${parameter.product}/${date}/${Fahrtnummer}`
+            const date = new Date(parameter.date).toLocaleTimeString("de-DE", {day: "2-digit", month: "2-digit", year: "numeric"}).split(",")[0];
+            url = `${this.api_url}/fahrten.json/${parameter.product}/${date}/${Fahrtnummer}`;
         }else{
-            url = `${this.api_url}/fahrten.json/${parameter.product}/${Fahrtnummer}`
-        }
+            url = `${this.api_url}/fahrten.json/${parameter.product}/${Fahrtnummer}`;
+        };
         return Fahrten.getTrips(url).then(function(Fahrten){
-            return Fahrten
+            return Fahrten;
         }).catch(function(err){
-            return err
-        })
-    }
+            return err;
+        });
+    };
 
     /**
      * @param {Number} product Fahrtnummer
      * @param {Object} parameter Quary parameter
     */
      getTrips(product, parameter){
-        let url = `${this.api_url}/fahrten.json/${product}`
+        let url = `${this.api_url}/fahrten.json/${product}`;
 
         if(parameter){
-			url = `${url}?${this.encodeQueryData(parameter, 'Trips')}`
-		}
+			url = `${url}?${this.encodeQueryData(parameter, "Trips")}`;
+		};
         return Fahrten.getTrips(url).then(function(Fahrten){
-            return Fahrten
+            return Fahrten;
         }).catch(function(err){
-            return err
-        })
-    }
+            return err;
+        });
+    };
 
     /**
      * Function to scrape the VAG Webpage to return all ongoing delays, elevator outages and planned events as a object
@@ -159,13 +159,13 @@ class openvgn {
      */
     getVagWebpageDisturbances(test){
         return WebProcessor.getVagWebpageDisturbances(test).then(function(Oobject){
-            return Oobject
+            return Oobject;
         }).catch(function(err){
-            return err
-        })
-    } 
+            return err;
+        });
+    };
 
-}
+};
 
 module.exports = {
 	openvgn
