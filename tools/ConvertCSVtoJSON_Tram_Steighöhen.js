93b944ee8e40dbed0b752b6dabeb71aa38fa6beb
Marc <bolverblitz@ebg.pw>
2022-03-08 03:37:13 +0100

    Added "Tram platform heights"

--- tools/ConvertCSVtoJSON_Tram_Steighöhen.js
+++ tools/ConvertCSVtoJSON_Tram_Steighöhen.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 const readline = require('readline');
 const path = require('path');
 const path_static = path.join(__dirname, '../static');
-let ErsteZeileArr = "";
+let ErsteZeileArr = [];
 
 const Filenames = {
-    "1": "Fuhrpark_Tram.json"
+    "1": "Steighoehen_Tram.json"
 }
 
 /* Functions */
@@ -39,7 +39,17 @@ function ParseCSV(dowhat, downloadlink) {
 
         const body_lines_array = body.split("\n")
 
-        ErsteZeileArr = body_lines_array[0].split(',');
+        const ErsteZeileArrTemp = body_lines_array[0].split(',');
+        for (i = 0; i < ErsteZeileArrTemp.length; i++) {
+            if(ErsteZeileArrTemp[i].includes('"position')){
+                ErsteZeileArrTemp[i] = ErsteZeileArrTemp[i].replace(/"/g, '');
+                ErsteZeileArr.push(`${ErsteZeileArrTemp[i]}.5`);
+            }else if(!ErsteZeileArrTemp[i].includes('5"')){
+                ErsteZeileArr.push(`${ErsteZeileArrTemp[i]}`);
+            }
+
+            
+        }
 
         for (i = 1; i < body_lines_array.length - 1; i++) {
 
@@ -70,6 +80,9 @@ function ParseCSV(dowhat, downloadlink) {
         const dowhat = await askQuestion("Welche Datei möchtest du verarbeiten?\n1: Tram Steighöhen\n> ");
         const downloadlink = await askQuestion("Nenne mir den aktuellen Downloadlink\n> ");
 
+        //const dowhat = 1;
+        //const downloadlink = "https://opendata.vag.de/datastore/dump/37516980-a1ce-461f-aa68-bf466a70e7fc?bom=True";
+        
         ParseCSV(dowhat, downloadlink);
 
     } catch (e) {
