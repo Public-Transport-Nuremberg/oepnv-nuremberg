4722e07ca87c821b5d4a8d4010f7ca0bd00c4cea
Marc <bolverblitz@ebg.pw>
2026-05-28 15:02:08 +0200

    feat: tram metadata expanded

--- static/index.js
+++ static/index.js
@@ -4,6 +4,15 @@ const Steighoehen_Tram = require("./steighoehen-tram");
 const StopInfo_Tram = require("./haltestellen-tram");
 const StopInfo_Ubahn = require("./bahnhoefe-u-bahn");
 const Fuhrpark_PVU = require("./PVU");
+const Tram_Types = require("./tram-types");
+
+
+// Itterate over the keys of Fuhrpark_Tram and add the corresponding values from Tram_Types
+for (const key in Fuhrpark_Tram) {
+    if (Tram_Types[Fuhrpark_Tram[key].typ]) {
+        Fuhrpark_Tram[key] = { ...Fuhrpark_Tram[key], ...Tram_Types[Fuhrpark_Tram[key].typ] };
+    }
+}
 
 module.exports = {
     Fuhrpark_Bus,
